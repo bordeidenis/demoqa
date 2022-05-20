7565e0d6fa5ea399ca8e44fd3493d543c315b09e
Denis Bordei <d.bordei@andersenlab.com>
2022-05-20 23:23:06 +0300

    first commit

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
